b9527b4d7fdf1392503f7ddf68ee9fc0cda8e929
Kenneth_Ciriaco <crownedtable@gmail.com>
2025-08-20 01:58:07 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,31 @@
+<section class="signup-container">
+    <div class="signup-form">
+      <h2>Create Your Account</h2>
+      <form>
+        <div class="form-grid">
+          <input type="text" placeholder="First Name">
+          <input type="text" placeholder="Middle Name">
+          <input type="text" placeholder="Last Name">
+
+          <input type="text" placeholder="Username" class="col-3">
+
+          <input type="email" placeholder="Email" class="col-2">
+          <input type="text" placeholder="Phone">
+
+          <input type="text" placeholder="Home Address" class="col-2">
+          <button type="button" class="upload-btn">Upload License</button>
+        </div>
+
+        <div class="checkboxes">
+          <label><input type="checkbox"> I agree to the Terms and Conditions</label>
+          <label><input type="checkbox"> Subscribe to newsletter</label>
+        </div>
+
+        <button type="submit" class="register-btn">Register</button>
+      </form>
+    </div>
+
+	<div class="signup-image">
+    <img src="C:/Users/LENOVO/Documents/Index/RegisterImage.png" alt="Signup Image">
+	</div>
+  </section>
