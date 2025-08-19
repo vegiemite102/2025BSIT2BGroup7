846c88ed249f44eda5690ea688673e15b53e65d0
Kenneth_Ciriaco <crownedtable@gmail.com>
2025-08-19 23:17:08 +0800

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,10 @@
+  <div class="nav">
+    <img class="logo" src="C:/Users/LENOVO/Documents/Index/Logo.png" alt="Logo">
+    <div class="link">
+      <a href="#" id="home">Home</a>
+      <a href="#" id="stats">Statistics</a>
+      <a href="#" id="map">Map</a>
+      <a href="#" id="about">About us</a>
+      <a href="#" id="settings">Setting</a>
+    </div>
+  </div>
