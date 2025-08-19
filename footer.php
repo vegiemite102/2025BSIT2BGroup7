6c45d50e9784bbd522b0624b09acb434feb8f1ff
Kenneth_Ciriaco <crownedtable@gmail.com>
2025-08-19 23:31:26 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer>
+    <div class="footer-container">   
+        <img class="logo" src="C:/Users/LENOVO/Documents/Index/Logo.png" alt="Pathfinder Logo">
+
+      <!-- App Store buttons -->
+      <div class="store-buttons">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" alt="Google Play">
+        <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="App Store">
+      </div>
+
+      <!-- Socials -->
+      <div class="social">
+        <p>FOLLOW US</p>
+        <div class="social-links">
+          <a href="#"><i class="fab fa-instagram"></i>@pathfinder.ph</a>
+          <a href="#"><i class="fab fa-facebook"></i>pathfinder.ph</a>
+          <a href="tel:09489674321"><i class="fas fa-phone"></i>0948 967 4321</a>
+          <a href="mailto:pathfinder@gmail.com"><i class="fas fa-envelope"></i>pathfinder@gmail.com</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-divider"></div>
+
+    <div class="footer-bottom">
+      <p>© 2025 PATHFINDER. All rights reserved.</p>
+      <p>
+        <a href="#">PRIVACY POLICY</a> | 
+        <a href="#">TERMS AND CONDITIONS</a>
+      </p>
+    </div>
+  </footer>	
